9a04c15f7e611ea5b58ad473165e514917ad6d07
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-11-25 18:08:26 -0300

    Finish challenge 01

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express"
+import { BracketsValidation } from "./controllers/BracketsController"
 
 const router = Router()
+
+// Brackets validation
+router.get("/brackets", BracketsValidation)
 
 export { router }
