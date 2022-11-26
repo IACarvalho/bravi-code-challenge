c08b6431ee9dda9b65f2c82f00e446a443c1f9a7
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-11-25 21:19:10 -0300

    Code refatoration

--- api/src/controllers/BracketsController.ts
+++ api/src/controllers/BracketsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { validation } from "../models/BracketsModel";
 
 
 function BracketsValidation(request: Request, response: Response){
@@ -15,67 +16,7 @@ function BracketsValidation(request: Request, response: Response){
   })
 }
 
-function validation(text: string): boolean {
-  const splitedText = text.split('')
 
-  if(
-    splitedText[0] === ']' 
-    || splitedText[0] === ')' 
-    || splitedText[0] === '}'
-  ){
-    return false
-  }
-
-  const parentheses = splitedText
-                        .filter(letter => letter === '(' || letter === ')')
-  
-  const squareBrackets = splitedText
-                        .filter(letter => letter === '[' || letter === ']')
-  
-  const brackets = splitedText
-                        .filter(letter => letter === '{' || letter === '}')
-  
-  if(parentheses.length > 0 && parentheses.length % 2 !== 0)
-    return false
-  
-  if(squareBrackets.length > 0 && squareBrackets.length % 2 !== 0)
-    return false
-  
-  if(brackets.length > 0 && brackets.length % 2 !== 0)
-    return false
-                      
-  if(!validateBracketsTypes(parentheses, '('))
-    return false
-  
-  if(!validateBracketsTypes(squareBrackets, '['))
-    return false
-
-  if(!validateBracketsTypes(brackets, '{'))
-    return false
-  
-  return true
-}
-
-// type is a left bracket (, [ or {
-function validateBracketsTypes(list: string[], type: string): boolean{
-  const leftPositions = []
-  const rightPositions = []
-
-  for(let i in list){
-    if(list[i] === type)
-      leftPositions.push(i)
-    else
-      rightPositions.push(i)
-  }
-
-
-  for(let i in leftPositions) {
-    if(Number(leftPositions[i]) > Number(rightPositions[i]))
-      return false
-  }
-
-  return true
-}
 
 
 
