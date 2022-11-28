c6ad50f4ed607e3712074ed75ee79032d45682c4
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-11-28 14:56:10 -0300

    User card

--- frontend/src/Components/UserCard/index.tsx
+++ frontend/src/Components/UserCard/index.tsx
@@ -1,5 +1,142 @@
-export function UserCard() {
+import { Trash, Pencil, PlusCircle } from "phosphor-react"
+
+import "./UserCard.scss"
+
+type Contact = {
+  id: number;
+  phoneNumber?: string;
+  email?: string;
+  whatsappNumber?: string;
+}
+
+type Props = {
+  userID: number;
+  name: string;
+  contacts: Contact[];
+}
+
+export function UserCard({ userID, name, contacts }: Props) {
+  function handleTrashClick() {
+    console.log(userID)
+  }
+
+  function handleEditButtonClick() {
+    console.log("Editar")
+  }
+
+  function handleDeleteContactClick() {}
+
+  function handleEditContactClick() {}
+
+  function handleCreateContactClick() {}
+
   return (
-    <h1>User Card</h1>
+    <div id="user-card-main-body">
+      <div className="user-card-header">
+        <span id="user-name">{name}</span>
+        <span className="button-group">
+          <Pencil
+            onClick={handleEditButtonClick}
+            className="user-card-button"
+            color="#b2b6ec"
+            size={32}
+            alt="Editar usuário"
+          />
+          <Trash
+            className="user-card-button"
+            onClick={handleTrashClick}
+            color="#c90a0a"
+            weight="fill"
+            size={32}
+            alt="Excluir usuário"
+          />
+        </span>
+      </div>
+      <div>
+        <div id="contact">
+            {contacts.map((contact: Contact) => {
+              return (
+                <ul>
+                  {!contact.phoneNumber ? null : (
+                    <li key={contact.id}>
+                      <b>Telefone:</b> {contact.phoneNumber}{" "}
+                      <span className="button-group">
+                        <Pencil
+                          onClick={handleEditContactClick}
+                          className="user-card-button"
+                          color="#b2b6ec"
+                          size={16}
+                          alt="Editar contato"
+                        />
+                        <Trash
+                          className="user-card-button"
+                          onClick={handleDeleteContactClick}
+                          color="#c90a0a"
+                          weight="fill"
+                          size={16}
+                          alt="Ecluir contato"
+                        />
+                      </span>
+                    </li>
+                  )}
+                  {!contact.email ? null : (
+                    <li key={contact.id}>
+                      <b>E-mail:</b> {contact.email}{" "}
+                      <span className="button-group">
+                        <Pencil
+                          onClick={handleEditContactClick}
+                          className="user-card-button"
+                          color="#b2b6ec"
+                          size={16}
+                          alt="Editar contato"
+                        />
+                        <Trash
+                          className="user-card-button"
+                          onClick={handleDeleteContactClick}
+                          color="#c90a0a"
+                          weight="fill"
+                          size={16}
+                          alt="Ecluir contato"
+                        />
+                      </span>
+                    </li>
+                  )}
+                  {!contact.whatsappNumber ? null : (
+                    <li key={contact.id}>
+                      <b>Whatsapp:</b> {contact.whatsappNumber}{" "}
+                      <span className="button-group">
+                        <Pencil
+                          onClick={handleEditContactClick}
+                          className="user-card-button"
+                          color="#b2b6ec"
+                          size={16}
+                          alt="Editar contato"
+                        />
+                        <Trash
+                          className="user-card-button"
+                          onClick={handleDeleteContactClick}
+                          color="#c90a0a"
+                          weight="fill"
+                          size={16}
+                          alt="Ecluir contato"
+                        />
+                      </span>
+                    </li>
+                  )}
+                </ul>
+              )
+            })}
+            <span>
+              <PlusCircle 
+                color="#099420" 
+                size={32} 
+                alt="Adicionar novo contato"
+                id="create-contact-button"
+                onClick={handleCreateContactClick}
+              />
+            </span>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/Pages/User/index.tsx
+++ frontend/src/Pages/User/index.tsx
@@ -1,9 +1,21 @@
 import { UserCard } from "../../Components/UserCard";
+import "./User.module.scss"
 
+const contacts = [
+  {
+    id: 1,
+    phoneNumber: "(85) 0 0000-0000"
+  },
+  {
+    id: 2,
+    email: "teste@email.com",
+    whatsappNumber: "(00) 0 0000-0000"
+  }
+]
 export function User() {
   return(
-    <div>
-      <UserCard />
-    </div>
+    <main>
+      <UserCard userID={1} name="Fulano de tal" contacts={contacts}/>
+    </main>
   )
 }
