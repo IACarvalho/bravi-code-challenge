071bc7de85218c9db883717f90779ef9f8092216
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-12-04 01:36:05 -0300

    Cors configuration

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,8 +2,11 @@ import "reflect-metadata"
 import express from "express"
 import { router } from "./routes"
 import { AppDataSource } from "./data-source"
+import cors from "cors"
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
